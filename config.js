bfe1f4830256e7740260be6ef779917b485f753e
HUNGNX <111565672+HUNGNX@users.noreply.github.com>
2024-04-04 14:35:37 +0700

    a
    a

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Máy bay à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao chị bay nhầm vào tim em lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích em <33',
+    btnNo: 'Không, em trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại gặp được chị <3',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Em biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
+    messDesc: 'Hẹn gặp chị sớm nhất nhó <3',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100048903353346' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
