931ad725b95922cdbb4a8073d6ee98184c2bed0a
HUNGNX <111565672+HUNGNX@users.noreply.github.com>
2024-09-28 09:34:04 +0700

    a

--- config.js
+++ config.js
@@ -1,28 +1,18 @@
 const CONFIG = {
-    introTitle: 'Máy bay à!',
-    introTitle: 'Linh à!',
+    introTitle: 'Em à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao chị bay nhầm vào tim em lẻ loi
-    Mà sao bạn bay nhầm vào tim tớ lẻ loi
+    Mà sao em bay nhầm vào anh tớ lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích em <33',
-    btnNo: 'Không, em trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại gặp được chị <3',
-    btnReply: 'Gửi cho em <3',
-    title: 'Phải chăng tớ đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng tớ đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích cậu <33',
-    btnNo: 'Không, cậu à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao tớ lại gặp được cậu <3',
-    btnReply: 'Gửi cho tớ <3',
+    btnNo: 'Không, anh à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại gặp được em <3',
+    btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Em biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
-    messDesc: 'Hẹn gặp chị sớm nhất nhó <3',
-    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
-    messDesc: 'Hẹn gặp cậu sớm nhất nhó <3',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Hẹn gặp em sớm nhất nhó <3',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/messages/t/100048903353346' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
